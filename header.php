15c2fddf4958699b2cebbcdd2029fce079a9a440
Md Saroar Jahan <43512597+saroarjahan@users.noreply.github.com>
2019-07-05 22:19:53 +0300

    header fix mobile resposnosev

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title>Task Mart</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 	<link rel="shortcut icon" type="image/png" href="./static/css/image/logo.jpg"/>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -18,32 +19,31 @@
 <body class="login">
 	<!--Navigation-->
 	<div class="nav-back">
-	<nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
-	  <a class="navbar-brand logo" href="index.php"><img src="./static/css/image/logo.png"></a>
-	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-	    <span class="navbar-toggler-icon"></span>
-	  </button>
-
-	  <div class="collapse navbar-collapse" id="navbarTogglerDemo02 topheader">
+		<nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+		  <a class="navbar-brand logo" href="index.php"><img src="./static/css/image/logo.png"></a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
 		    <ul class="navbar-nav nav mr-auto mt-2 mt-lg-0">
-		      <li class=" nav-item">
-		        <a class="nav-link" href="index.php">Our Experiments</a>
-		      </li>
-		      
-		      <li class="nav-item">
-		        <a class="nav-link" href="Findings_&_Data_Sets.php">Findings & Data Sets</a>
-		      </li>
-		      <li class="nav-item">
-		        <a class="nav-link" href="for_researchers.php">For Researchers</a>
-		      </li>
-		      <li class="nav-item">
-		        <a class="nav-link disabled" href="about_us.php">About Us</a>
-		      </li>
-		    </ul>
+			      <li class=" nav-item">
+			        <a class="nav-link" href="index.php">Our Experiments</a>
+			      </li>
+			      
+			      <li class="nav-item">
+			        <a class="nav-link" href="Findings_&_Data_Sets.php">Findings & Data Sets</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="for_researchers.php">For Researchers</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link disabled" href="about_us.php">About Us</a>
+			      </li>
+			</ul>
 		    <form class="form-inline my-2 my-lg-0">
-		      <input class="form-control mr-sm-2" type="search" placeholder="Search">
+		      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
 		      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
 		    </form>
-	  </div>
-	</nav>
+		  </div>
+		</nav>
     </div>
