584d9e272b2ca9b29f4f59b56e9097c086e167a8
Md Saroar Jahan <43512597+saroarjahan@users.noreply.github.com>
2019-07-16 19:24:41 +0300

    admin

--- php/taskedit.php
+++ php/taskedit.php
@@ -1,8 +1,8 @@
-
 <form action="taskedit.php" method="post">
 	Title: <input type="text" name="title"><br>
 	Description: <input type="text" name="description"><br>
-	url: <input type="text" name="url"><br>
+	Reward: <input type="number" name="reward"><br>
+	Url: <input type="text" name="url"><br>
 	<input type="submit" name="submit">
 </form>
 
@@ -16,9 +16,10 @@ if (isset($_POST['submit'])) {
   $title = $_POST["title"];
   $description = $_POST["description"];
   $url = $_POST["url"];
+  $reward= $_POST["reward"];
 
-  $sql = "INSERT INTO tasks (title,url)
-  VALUES ('$title', '$url')";
+  $sql = "INSERT INTO tasks (title,description,url,reward,image)
+  VALUES ('$title','$description','$url','$reward','http://taskmart.online/static/css/image/task.jpg')";
 
 	if (mysqli_query($conn, $sql)) {
 	    echo "New record created successfully";
@@ -26,9 +27,6 @@ if (isset($_POST['submit'])) {
 	    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 	}
 }
-
-
-
 
 mysqli_close($conn);
 ?>
