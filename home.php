6453502927caa086e0a1ff28ba808795365d17e2
Md Saroar Jahan <43512597+saroarjahan@users.noreply.github.com>
2019-09-03 05:10:37 +0300

    user login cookies zero complete

--- home.php
+++ home.php
@@ -16,12 +16,15 @@ include './auth/index.php';
 		<div class="row">
 			<div class="col-md-3" v-for="data in info.data">
  				 <h5 class="empty task_title">{{data.title}}</h5>
- 				 <a target="_blank" :href="data.URL"><img :src="data.image"></a>
- 				 <p class="des maintaks">{{data.description}}</p>
- 				 	<div v-if="task_secretes.includes(data.secrete)">
-					  <button class="btn btn-warning btn-block" :id="complete">Taks Completed <i class="fas fa-check-double"></i></button>
+ 				 
+ 				 	<div v-if="task_secretes.includes(data.secrete)">	  
+					  <img :src="data.image">
+ 				 	  <p class="des maintaks">{{data.description}}</p>
+ 				 	  <button class="btn btn-warning btn-block" :id="complete">Taks Completed <i class="fas fa-check-double"></i></button>
 					</div>
 					<div v-else>
+					  <a target="_blank" :href="data.URL"><img :src="data.image"></a>
+ 				 	  <p class="des maintaks">{{data.description}}</p>
 					  <a target="_blank" :href="data.URL"><button class="btn btn-info btn-block">Click To Participate</button></a>
 					</div>				  
 			</div>    
